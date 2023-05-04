c2f473f99c2a2b173496de63e718a072e28fac85
Prasanti <nalla.prasanti@gmail.com>
2023-05-04 14:50:31 +0100

    Final version of code

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,5 @@
 import { Route, Routes } from 'react-router-dom';
-import { Home } from './components/Home/Home';
-import { WeatherByLocation } from './components/Location/WeatherByLocation';
 import './App.scss';
-import { WeatherByLatLon } from './components/WeatherByLatLon/WeatherByLatLon';
 import { Forecast } from './components/Forecast/Forecast';
 import MapChart from './components/Map/UKMap';
 
@@ -11,13 +8,10 @@ function App() {
   return (
     <main>
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/location" element={<WeatherByLocation />} />
-        <Route path="/latlon" element={<WeatherByLatLon />} />
-        <Route path="/forecast" element={<Forecast />} />
-        <Route path="/map" element={<div style={{ height: "100vh" }}>
+        <Route path="/" element={<Forecast />} />
+        {/* <Route path="/map" element={<div style={{ height: "100vh" }}>
           <MapChart />
-        </div>} />
+        </div>} /> */}
       </Routes>
     </main>
   )
